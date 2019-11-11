584e805949ea134ceb019f8321b3dae112dd86fa
Igor Knop <igorknop@gmail.com>
2019-11-11 19:55:17 -0300

    Adiciona setup de teclado

--- InputController.js
+++ InputController.js
@@ -3,3 +3,28 @@ function InputController() {
     this.codigos = {};
     this.teclas = {};
 }
+
+InputController.prototype.setupKeyboard = function(novasTeclas){
+    for (var t = 0; t < novasTeclas.length; t++) {
+        const tecla = novasTeclas[t];
+        this.nomes[tecla.codigo] = tecla.nome;
+        this.codigos[tecla.nome] = tecla.codigo;
+        this.teclas[tecla.nome] = false;        
+    }
+    var that = this;
+    addEventListener("keydown", function (e) {
+        var nome = that.nomes[e.keyCode];
+        if(nome){
+            that.teclas[nome] = true;
+        }
+         
+     });
+     addEventListener("keyup", function (e) {
+        var nome = that.nomes[e.keyCode];
+        if(nome){
+            that.teclas[nome] = false;
+        }
+         
+     });
+  
+}
